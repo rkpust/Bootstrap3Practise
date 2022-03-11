20305cb176e12407da2cd5690eb2bf7ba3152e1d
RK <rezaul.cse.pust17@gmail.com>
2022-03-11 09:38:56 +0600

    It is an index page that show all pages link at a glance

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,  initial-scale=1">
+	<title>Welcome to Bootstrap 3 Practise</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/vendor/jquery-1.12.0.min.js"></script>
+</head>
+	<body>
+		<div class="container"> 
+		<h2>Bootstrap 3 At A Galnce</h2>
+		<h3>What is Bootstrap?</h3>
+		<p>
+			<ol>
+				<li>Bootstrap is a free front-end framework for faster and easier web development</li>
+				<li>Bootstrap includes HTML and CSS based design templates for typography, forms, buttons, tables, navigation, modals, image carousels and many other, as well as optional JavaScript plugins</li>
+				<li>Bootstrap also gives you the ability to easily create responsive designs</li>
+			</ol>
+		</p>
+		<div class="btn-group">
+
+		<a href="basicBSpage1.php"> <button type="button" class="
+			btn btn-primary">Basic Page 1</button> </a>
+
+		</div>
+		</div>
+	</body>
+
+
+    
+</html>
