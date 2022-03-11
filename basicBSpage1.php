2d838be6959bba5a7d1ff1bd56a002efd0a830f5
RK <rezaul.cse.pust17@gmail.com>
2022-03-11 10:19:26 +0600

    First Bootstrap Page

---
+++ basicBSpage1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,  initial-scale=1">
+	<title>BS First Page</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/vendor/jquery-1.12.0.min.js"></script>
+</head>
+
+<body>
+	<div class="container"> 
+		<h1>My First Bootstrap Page.</h1>
+		<p>This is some text.</p>
+		<p>This part is inside a .container class.</p>
+		<p>The .container class provides a responsive fixed width container.</p>
+		
+	</div>
+</body>
+
+
+    
+</html>
