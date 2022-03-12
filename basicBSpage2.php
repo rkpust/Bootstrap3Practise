1cba600f4eb1169c1e70f53504db9b67c4881891
RK <rezaul.cse.pust17@gmail.com>
2022-03-12 22:42:04 +0600

    Second Bootstrap Page

---
+++ basicBSpage2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,  initial-scale=1">
+	<title>BS Second Page</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/vendor/jquery-1.12.0.min.js"></script>
+</head>
+
+<body>
+	<div class="container-fluid"> 
+		<h1>My Second Bootstrap Page.</h1>
+		<p>This is some text.</p>
+		<p>This part is inside a .container-fluid class.</p>
+		<p>The .container-fluid class provides a full width container, spanning the entire width of the viewport.</p>
+		
+	</div>
+</body>
+
+
+    
+</html>
